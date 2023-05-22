068fd145cec45808ec10e9af717b7cff39b6afdc
Tan-cntt5 <125740918+Tan-cntt5@users.noreply.github.com>
2023-05-22 14:01:01 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main{
+    public static void Main(String[] args){
+        MayTinhGaming may
+    }
+}
